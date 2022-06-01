52e50542e45e8582774c079df963a052a216cfb7
kim <kha@rapa.edu>
2022-06-02 04:22:56 +0900

    두번때 커밋

--- findgrep.sh
+++ findgrep.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
 pattern=$1
+directory=$2
+if [ -z "$directory" ]
+then
+	directory="."
+fi
 
-find . -type f | xargs grep -nH "$pattern"
+find "$directory". -type f | xargs grep -nH "$pattern"
