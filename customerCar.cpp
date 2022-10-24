3555ba8b9ef2e0243997fb7a613ab1cc521473b2
moon <ansgoals1@naver.com>
2022-10-24 12:22:00 +0900

    change last

--- customerCar.cpp
+++ customerCar.cpp
@@ -35,6 +35,7 @@ void BFS(int x, int y,int(*visit)[70], int(*check)[70], int (*map)[70],vector<pa
     int originY= y;
     int dx[4] = {1,0,-1,0};
     int dy[4] = {0,1,0,-1};
+    pair<int,int> mother[30][70];
     visit[x][y]=1;
     queue<pair<int,int>> q;
     q.push(make_pair(x,y));
@@ -49,6 +50,7 @@ void BFS(int x, int y,int(*visit)[70], int(*check)[70], int (*map)[70],vector<pa
                 if(map[nX][nY]==0 && visit[nX][nY]==0){ //map 통과 가능시, visit없을시
                     check[nX][nY]=check[x][y]+1;
                     visit[nX][nY]=1;
+                    mother[nX][nY]=make_pair(x,y);
                     q.push(make_pair(nX,nY));
                 }
             }
@@ -63,6 +65,12 @@ void BFS(int x, int y,int(*visit)[70], int(*check)[70], int (*map)[70],vector<pa
             MinX = G.first;
             MinY = G.second;
             }
+    }
+    vector<pair<int,int>> tracking;
+    tracking.push_back(make_pair(MinX,MinY));
+    while(1){
+        tracking.push_back(mother[tracking.back().first][tracking.back().second]);
+        if(tracking.back().first==originX && tracking.back().second==originY) break;
     }
     cout<<"===========================================================================";
     cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
@@ -131,6 +139,30 @@ void BFS(int x, int y,int(*visit)[70], int(*check)[70], int (*map)[70],vector<pa
             printf("가 현재 위치 ,");
             printf("\033[0;31m레드\033[0m");
             printf("가 목적지 입니다.\n");
+            getchar();
+            gotoxy(0,0);
+            for(int i=0;i<30;i++){
+                for(int j=0;j<70;j++){
+                    if(i==0&&j==0)cout<<endl;
+                    int flag =0;
+                    if(i==MinX&&j==MinY){printf("\033[0;31m♥\033[0m"); continue;}
+                    for(auto V : asPosition){
+                        if(i==V.first&&j==V.second){printf("\033[0;33mO\033[0m"); flag =1; continue;}
+                    }
+                    if(flag==1)continue;
+                    if(i==originX && j==originY){printf("\033[0;34m♥\033[0m"); continue;}
+                    for(auto V : tracking){
+                        if(i==V.first&&j==V.second){
+                            printf("\033[0;33m♥\033[0m"); flag =1; continue;}
+                    }
+                    if(flag==1)continue;
+                    if(map[i][j]==0) printf("\033[0;30mO\033[0m");
+                    else printf("\033[0;32mO\033[0m");
+                }
+
+                cout<<endl;
+
+            }
             getchar();
             break;
         default:
